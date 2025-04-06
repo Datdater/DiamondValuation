3c0e7c5dc6b016c095d5fbb565906ed5073ab207
Dat Nguyen Quoc <datnqse173081@fpt.edu.vn>
2025-04-06 10:35:39 +0700

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import AuthService from "./auth.service.js";
 
-export const beURL = "http://shourai.io.vn/api/v1/";
+export const beURL = "https://prn-222.food/api/v1/";
 
 export const http = axios.create({
   //baseURL: "http://localhost:8080/api/v1/",
